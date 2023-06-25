b2d2c81a1146c031feed3c86d4e5247c78c3e348
karlarios6534 <karla.rios6534@alumnos.udg.mx>
2023-06-25 12:39:43 -0600

    change format

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 async def hola(variables:str):
     try:
         variables=variables.replace('{','').replace('}','')
-        variables = list(map(float, variables.split(",")))
+        variables = list(map(float, variables.split("%")))
         new_variables = []
         indexes=[0,3,6,9,12,15,18,21,24,27,1,4,7,10,13,16,19,22,25,28,2,5,8,11,14,17,20,23,26,29]
         new_variables=[]
